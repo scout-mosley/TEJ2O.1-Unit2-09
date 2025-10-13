fc5418d0d77d5b2b866c902a8b0ab25f282ad2f6
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-10-13 11:39:05 -0400

    adds one to score and shows score

--- main.ts
+++ main.ts
@@ -4,43 +4,50 @@
  * Created on: oct 2025
  * This program plays rock paper scissor's 
 */
-
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+game.setScore(0)
 
-let randomNumber = randint(0, 2)
+// When shake, pick a new random number each time
+input.onGesture(Gesture.Shake, function () {
+    let randomNumber = randint(0, 2)
 
-//when shake pick random number
-input.onGesture(Gesture.Shake, function() {
-    randomNumber
-
-    // if randomNumber was 0
+    // if randomNumber was 0 → Rock
     if (randomNumber == 0) {
         basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # # # .
-        # . . . #
-        # . . . #
+            . . . . .
+            . . . . .
+            . # # # .
+            # . . . #
+            # . . . #
         `)
     }
 
-    // if randomNumber was 1
+    // if randomNumber was 1 → Paper
     if (randomNumber == 1) {
         basic.showIcon(IconNames.Square)
     }
 
-    // if randomNumber was 2
+    // if randomNumber was 2 → Scissors
     if (randomNumber == 2) {
         basic.showIcon(IconNames.Scissors)
     }
 
-    // pasue and show you the output
+    // pause and reset
     basic.pause(1000)
     basic.showIcon(IconNames.Happy)
 })
 
-//add 1 to score
+// add 1 to score with Button A
 input.onButtonPressed(Button.A, function () {
+    game.addScore(1)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(500)
+    basic.showIcon(IconNames.Yes)
+})
 
+// shows score
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(game.score())
+    basic.showIcon(IconNames.Happy)
 })
